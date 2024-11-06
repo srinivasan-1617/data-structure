27fde7261a656a712d089102eff87acf6065b494
srinivasan-1617 <iamsrini168@gmail.com>
2024-11-06 11:34:22 +0530

    Create Median of Two Sorted Arrays.c

---
+++ Median of Two Sorted Arrays.c
@@ -0,0 +1,30 @@
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2,
+                              int nums2Size) {
+    int m = nums1Size, n = nums2Size;
+    int p1 = 0, p2 = 0;
+    int getMin() {
+        if (p1 < m && p2 < n) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < m) {
+            return nums1[p1++];
+        } else if (p2 < n) {
+            return nums2[p2++];
+        }
+        return -1;
+    }
+
+    double median;
+    if ((m + n) % 2 == 0) {
+        for (int i = 0; i < ((m + n) / 2) - 1; ++i) {
+            int temp = getMin();
+        }
+        median = (getMin() + getMin()) / 2.0;
+    } else {
+        for (int i = 0; i < (m + n) / 2; ++i) {
+            int temp = getMin();
+        }
+        median = getMin();
+    }
+
+    return median;
+}
